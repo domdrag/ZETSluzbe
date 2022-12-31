2963985a07d2630d8588da1355e7d5d222d0f037
domdrag <pasjekonj@gmail.com>
2022-12-31 10:54:00 +0100

    updateCall4

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from kivy.core.clipboard import Clipboard
 
 from plyer import call
 
+from jnius import autoclass
+from jnius import cast                      
+
 from readServices import *
 from receiveServices import *
 from repair import *
@@ -47,8 +50,15 @@ class CallInfoPromptPopup(Popup):
         Clipboard.copy(self.phoneNumber)
             
     def callNumber(self):
-        call.makecall(tel = self.phoneNumber)
-
+        #call.makecall(tel = self.phoneNumber)
+        Intent = autoclass('android.content.Intent')
+        Uri = autoclass('android.net.Uri')          
+        PythonActivity = autoclass('org.renpy.android.PythonActivity')                                        
+        intent = Intent(Intent.ACTION_CALL)         
+        intent.setData(Uri.parse("tel:" + "0997686709"))     
+        currentActivity = cast('android.app.Activity', PythonActivity.mActivity)                                                   
+        currentActivity.startActivity(intent)
+    
     def saveContact(self):
         pass
 
--- simpleKivyProgram/main.py
+++ simpleKivyProgram/main.py
@@ -1,14 +1,75 @@
-import kivy
-
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.clock import mainthread, Clock
+
+gui = '''
+Root:
+    orientation: 'vertical'
+
+    arena: arena
+    control_button: control_button
+
+    Arena:
+        id: arena
+
+    Button
+        id: control_button
+        size_hint_y: None
+        height: dp(50)
+        text: 'move'
 
 
-class MyApp(App):
+<Arena@FloatLayout>:
+    player: player
+
+    Button:
+        id: player
+        pos: 150, 300
+        text: 'elf warrior\\nlevel 1'
+        size_hint: None, None
+        size: 100, 100
+'''
+
+
+class Root(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        @mainthread
+        def job():
+            self.control_button.bind(on_press=self._on_press)
+            self.control_button.bind(on_release=self._on_release)
+
+        job()
+
+    def _on_press(self, button):
+        self.arena.start_movement()
+
+    def _on_release(self, button):
+        self.arena.stop_movement()
+
+
+class Arena(FloatLayout):
+
+    def start_movement(self):
+        Clock.schedule_interval(self._move_right, 0.01)
+
+    def stop_movement(self):
+        Clock.unschedule(self._move_right)
+
+    def _move_right(self, dt):
+        self.player.x += 1
+
+
+class Test(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return Builder.load_string(gui)
 
 
-if __name__ == '__main__':
-    MyApp().run()
+Test().run()
