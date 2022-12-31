c5e6caa43d3252d899e9b75ba02805a0f91ae0f2
domdrag <pasjekonj@gmail.com>
2022-12-31 20:13:25 +0100

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 
 public class Contact{
     public static void addContact(Activity pyActivity, String name, String phone){
-        Intent i = new Intent(Intent.ACTION_INSERT_OR_EDIT);
+        Intent i = new Intent(Intent.ACTION_INSERT);
         i.setType(Contacts.CONTENT_TYPE);
         i.putExtra(Intents.Insert.NAME, name);
         i.putExtra(Intents.Insert.PHONE, phone);
