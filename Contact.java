2b354659525b4c20a09296392e6bf88657fd2b5c
domdrag <pasjekonj@gmail.com>
2022-12-31 20:29:52 +0100

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -7,7 +7,7 @@ import android.net.Uri;
 public class Contact{
     public static void addContact(Activity pyActivity, String name, String phone){
         Intent intent = new Intent(Intents.SHOW_OR_CREATE_CONTACT, Uri.parse("tel:" + phone));
-        intent.putExtra(ContactsContract.Intents.EXTRA_FORCE_CREATE, true);
+        intent.putExtra(Intents.EXTRA_FORCE_CREATE, true);
         //intent.putExtra(Intents.Insert.NAME, name);
         //intent.putExtra(Intents.Insert.PHONE, phone);
         pyActivity.startActivity(intent);	
