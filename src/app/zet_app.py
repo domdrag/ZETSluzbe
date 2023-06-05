f1f907cd02b13dfb0543e8e1f10e80e6c30a3dd0
domdrag <pasjekonj@gmail.com>
2023-06-05 18:08:12 +0200

    md update

--- src/app/zet_app.py
+++ src/app/zet_app.py
@@ -6,4 +6,7 @@ from src.screen.zet_screen_manager import ZETScreenManager
 
 class ZETApp(MDApp):
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        #self.theme_cls.accent_palette = "Red"
         return Builder.load_file('design/screen/zet_screen_manager.kv')
