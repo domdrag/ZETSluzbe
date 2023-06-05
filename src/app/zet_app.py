3fec4d31c10db9f9a4b550f7786dfa14157b5887
domdrag <pasjekonj@gmail.com>
2023-06-05 17:39:37 +0200

    md tryon

--- src/app/zet_app.py
+++ src/app/zet_app.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivymd.app import MDApp
 
 from src.screen.zet_screen_manager import ZETScreenManager
 
-class ZETApp(App):
+class ZETApp(MDApp):
     def build(self):
         return Builder.load_file('design/screen/zet_screen_manager.kv')
