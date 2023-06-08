4216b471c7fca8a0de11cdadb79e105643b14aff
domdrag <pasjekonj@gmail.com>
2023-06-08 19:20:07 +0200

    md branch 1st commit

--- src/app/zet_app.py
+++ src/app/zet_app.py
@@ -3,6 +3,8 @@ from kivy.lang import Builder
 
 from src.screen.zet_screen_manager import ZETScreenManager
 
+# novi branch 2
+
 class ZETApp(App):
     def build(self):
         return Builder.load_file('design/screen/zet_screen_manager.kv')
