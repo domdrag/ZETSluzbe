eae98199447b832dca201d9a123c21d62b7c72b7
domdrag <pasjekonj@gmail.com>
2023-06-08 19:17:22 +0200

    Revert "Merge pull request #1 from domdrag/MDAppBranch"
    This reverts commit 42839da7422cb9768749fe0e7d58186ec6196541, reversing
    changes made to 9fc146aca947fc3c24e6266f85f104d1c1ce4ed2.

--- src/app/zet_app.py
+++ src/app/zet_app.py
@@ -3,8 +3,6 @@ from kivy.lang import Builder
 
 from src.screen.zet_screen_manager import ZETScreenManager
 
-# novi branch
-
 class ZETApp(App):
     def build(self):
         return Builder.load_file('design/screen/zet_screen_manager.kv')
