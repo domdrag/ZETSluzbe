d5df28894b2c08b94a5809ddfb2c8fe1f48da0e0
domdrag <pasjekonj@gmail.com>
2023-06-18 14:06:13 +0200

    md try

--- src/screen/main/tabs/services/utils/daily_service.py
+++ src/screen/main/tabs/services/utils/daily_service.py
@@ -1,6 +1,12 @@
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.properties import ColorProperty
+from kivy.properties import ColorProperty, StringProperty, ObjectProperty
 
 class DailyService(MDBoxLayout):
-    pass
+    day: ObjectProperty()
+    service: ObjectProperty()
+    bg_color: ObjectProperty()
+    
+    def __init__(self, **kwargs):
+        super(DailyService, self).__init__(**kwargs)
+
 
