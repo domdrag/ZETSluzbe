6352a8a64183ce2287a705482d1f30a19a21192b
domdrag <pasjekonj@gmail.com>
2023-06-20 19:48:39 +0200

    Update repair_all_files.py

--- src/data/share/repair_all_files.py
+++ src/data/share/repair_all_files.py
@@ -1,5 +1,11 @@
-from distutils.dir_util import copy_tree
+#from distutils.dir_util import copy_tree 
+import shutilt # distutils has been deprecated in 3.10 or 3.12
 
 def repairAllFiles():
+    shutil.copytree('data/backup',
+                    'data/data')
+
+'''
+def repairAllFiles():
     copy_tree('data/backup',
-              'data/data')
+              'data/data')'''
