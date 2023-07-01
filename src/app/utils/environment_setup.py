3f41a5e6f3b0b48c2de2124880c0ab623f55378b
domdrag <pasjekonj@gmail.com>
2023-07-01 20:57:11 +0200

    update

--- src/app/utils/environment_setup.py
+++ src/app/utils/environment_setup.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 from src.data.share.config_manager import getConfig
+from src.data.share.update_backup_dir import updateBackupConfig
 
 def environmentSetup():
     config = getConfig()
@@ -10,6 +11,8 @@ def environmentSetup():
         os.environ['KIVY_NO_CONSOLELOG'] = '1'
     if (not config['DEBUG_LOGS']):
         logging.disable(logging.DEBUG)
+    if (config['UPDATE_SUCCESSFUL']): # mind at ease when changing config
+        updateBackupConfig()
         
     os.environ['KIVY_ORIENTATION'] = config['APP_ORIENTATION']
 
--- src/data/share/update_backup_dir.py
+++ src/data/share/update_backup_dir.py
@@ -1,7 +1,10 @@
 from distutils.dir_util import copy_tree
 import shutil
 
-def updateBackupDir():
-    copy_tree('data/data', 'data/backup/data')
+def updateBackupConfig():
     shutil.copyfile('data/config.json', 'data/backup/config.json')
+
+def updateBackupDir():
+    updateBackupConfig()
+    copy_tree('data/data', 'data/backup/data')
 
