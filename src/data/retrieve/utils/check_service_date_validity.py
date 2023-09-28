b65d75b7a8eb609ede7672c8f97f1c92a4336ef7
domdrag <58782481+domdrag@users.noreply.github.com>
2023-09-28 19:35:14 +0200

    remove unneeded date libraries

--- src/data/retrieve/utils/check_service_date_validity.py
+++ src/data/retrieve/utils/check_service_date_validity.py
@@ -1,12 +1,9 @@
-import datetime
-
-from dateutil.relativedelta import relativedelta, FR
-from datetime import date
+from datetime import datetime, date
 
 from src.data.retrieve.utils.get_service_date import getServiceDate
 
 def getTodayDate():
-    now = datetime.datetime.now()
+    now = datetime.now()
     todayDate = date(now.year, now.month, now.day)
     return todayDate
 
