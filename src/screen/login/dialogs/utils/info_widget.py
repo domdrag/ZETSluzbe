c1ef0971591576b111b606f64fca044438845731
domdrag <58782481+domdrag@users.noreply.github.com>
2023-09-30 15:25:16 +0200

    update

--- src/screen/login/dialogs/utils/info_widget.py
+++ src/screen/login/dialogs/utils/info_widget.py
@@ -1,9 +1,9 @@
 from kivy.properties import StringProperty
 from kivymd.uix.relativelayout import MDRelativeLayout
 
-class LogsWidget(MDRelativeLayout):
+class InfoWidget(MDRelativeLayout):
     logs = StringProperty()  # binding
 
     def __init__(self, logs, **kwargs):
-        super(LogsWidget, self).__init__(**kwargs)
+        super(InfoWidget, self).__init__(**kwargs)
         self.logs = logs
