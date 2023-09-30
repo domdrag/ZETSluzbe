92feff111ca24c21c0c79ba8579ba3e3fb351ecf
domdrag <58782481+domdrag@users.noreply.github.com>
2023-09-30 11:35:56 +0200

    not test 3

--- data/data/notificationsFiles/test.py
+++ data/data/notificationsFiles/test.py
@@ -4,6 +4,4 @@ PDF = pdfplumber.open('Obavijest.pdf')
 page = PDF.pages[0]
 
 image = page.to_image(resolution = 300)
-text = 'UTF ENCODING ČČčč'
-encodedText = text.encode('utf8')
-image.save(encodedText + '.png')
+image.save('UTF ENCODING ČsČčč.png', encoding = 'utf-8')
